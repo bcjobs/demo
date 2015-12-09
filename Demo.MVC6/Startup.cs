3ec06e31e40aa377c2d9db458c8df8d03fdb0b9f
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-09 10:55:29 -0800

    Registered Autofac in Demo.MVC

--- Demo.MVC6/Startup.cs
+++ Demo.MVC6/Startup.cs
@@ -13,9 +13,10 @@ namespace Demo.MVC6
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            return AutofacConfig.Register(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
