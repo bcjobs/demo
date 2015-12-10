62c61b87ce636b80cd9604bd83e7af01df60295e
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-09 17:52:24 -0800

    Autofac / EventHandler testing.

--- Demo.MVC6/Controllers/Web/HomeController.cs
+++ Demo.MVC6/Controllers/Web/HomeController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Infra.Events;
 
 namespace Demo.MVC6.Controllers.Web
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            await new Services.Events.Base().RaiseAsync();
+            await new Services.Events.Derived().RaiseAsync();
+
             return Content(Startup.Configuration["AppSettings:Greeting"]);
         }
     }
