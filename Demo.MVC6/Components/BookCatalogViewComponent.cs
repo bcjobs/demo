5bb6ae650521808ee08f1e48527e7debb3cf5de9
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-10 12:23:48 -0800

    First view component.

---
+++ Demo.MVC6/Components/BookCatalogViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BookStore;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.MVC6.Components
+{
+    public class BookCatalogViewComponent : ViewComponent
+    {
+        public BookCatalogViewComponent(IBookCatalog catalog)
+        {
+            Catalog = catalog;
+        }
+
+        IBookCatalog Catalog { get; }
+
+        public IViewComponentResult Invoke(int max)
+        {
+            var books = Catalog.Read(new BookQuery
+            {
+                PageSize = max
+            });
+
+            return View(books);
+        }
+    }
+}
