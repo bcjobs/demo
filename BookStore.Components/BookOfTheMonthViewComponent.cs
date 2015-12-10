3c078ec0b527cd14f0d79d90fd446fe4982093f2
Johnny Oshika <johnnyoshika@gmail.com>
2015-12-10 14:25:29 -0800

    Added BookOfTheMonth view component

---
+++ BookStore.Components/BookOfTheMonthViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Components
+{
+    public class BookOfTheMonthViewComponent : ViewComponent
+    {
+        public BookOfTheMonthViewComponent(IBookCatalog catalog)
+        {
+            Catalog = catalog;
+        }
+
+        IBookCatalog Catalog { get; }
+
+        public IViewComponentResult Invoke()
+        {
+            var book = Catalog.Read(new BookQuery()).First();
+            return View(book);
+        }
+    }
+}
